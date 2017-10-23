a1e94fdf36a8fa5e06a2f7f18402fadf2082e919
Matias Alexander Rømer <mati0681@edu.easj.dk>
2017-10-23 11:26:19 +0200

    Lavet 2 unit tests for nu, som tjekker om metoderne virker

---
+++ MockExamBoxVolumeTests/Service1Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockExamBoxVolume;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockExamBoxVolume.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetSideTest()
+        {
+            Service1 Test = new Service1();
+            double gettest = Test.GetSide(8, 2, 2);
+            Assert.AreEqual(gettest, 2);
+        }
+
+        [TestMethod()]
+        public void GetVolumeTest()
+        {
+            Service1 Test = new Service1();
+            double volumetest = Test.GetVolume(2, 2, 2);
+            Assert.AreEqual(volumetest, 8);
+        }
+    }
+}
